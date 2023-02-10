9a6f9f2037beeb69e40a9142afec68baf8ceb561
Manuel Vera <manuelvera202@gmail.com>
2023-02-10 16:06:04 -0300

    Add run and idle animation to player tsc, now can run

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,17 +6,23 @@ const FRICTION = 500.0
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
