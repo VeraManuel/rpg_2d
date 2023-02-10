ee9bc6522d0c602e587de51d4229fc86d97d1ec1
Manuel Vera <manuelvera202@gmail.com>
2023-02-10 10:58:34 -0300

    add player and bush as scene to separeta behavior and make world to Ysort

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10.0
-const MAX_SPEED = 100.0
-const FRICTION = 10.0
+const ACCELERATION = 500.0
+const MAX_SPEED = 80.0
+const FRICTION = 500.0
 
 var velocity = Vector2.ZERO
 
@@ -11,14 +11,12 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	# this if is out of tutorial 1
 	if input_vector.length() > 0:
 		input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.limit_length(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
